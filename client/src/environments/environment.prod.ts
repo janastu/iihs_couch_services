9f5217eecd5064c083aa0290572066afa65885f0
Bhanu <salus-sage@users.noreply.github.com>
2018-01-11 17:22:48 +0530

    removing credentials

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  username: 'admin', //couchdb username
-  password: '!!hs2018', //couchdb password
+  username: 'db_username', //couchdb username
+  password: 'db_password', //couchdb password
   host: 'login.test.openrun.net', //couchdb host
   superLoginServerUrl: 'http://client.test.openrun.net:3001' //Super login server url(UI url)
 };
