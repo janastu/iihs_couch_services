0c252ca5714f3042c41ae325869a86ff358f73dd
vaishnavi <vaishnavisv001@gmail.com>
2018-03-22 10:23:52 +0530

    pull master

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   username: 'admin', //couchdb username
   password: 'admin', //couchdb password
-  host: '192.168.99.100:5984' , //couchdb host
-  superLoginServerUrl: 'http://192.168.99.100:3000', //Super login server url(UI url)
-  feedParserServiceUrl: 'http://192.168.99.100:3500' //Feed Parser Service URL
+  host: '172.18.0.3:5984' , //couchdb host
+  superLoginServerUrl: 'http://172.18.0.4:3000', //Super login server url(UI url)
+  feedParserServiceUrl: 'http://172.18.0.3:3500' //Feed Parser Service URL
 };
